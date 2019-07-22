d165fe8dffbcbe0e87465f09936270ec59aa67a3
enrico76 <enricodip@gmail.com>
2019-07-22 08:19:54 +0200

    flex

--- myscript.js
+++ myscript.js
@@ -1,1 +1,15 @@
 console.log("initial script");
+
+$( document ).ready(function() {
+
+
+  $(".container").click(
+     function(){
+    
+          $(this).children('.drop').toggleClass('active');
+          console.log($(this).children('.drop'));
+     }
+  );
+
+
+});
